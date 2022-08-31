19119ac9c3985ebfa81068004f7d5a3d3827edf8
forIinRanger <ZhuraMax2006@yandex.ru>
2022-08-31 20:54:22 +0500

    train

--- train.py
+++ train.py
@@ -13,13 +13,12 @@ args = parser.parse_args()
 file_to_save_model = args.model
 texts = []
 if args.input_dir != 'stdin':
-    dir_with_texts = args.input_dir
-    for name in os.listdir(f'{dir_with_texts}'):
-        with open(fr'{dir_with_texts}\{name}', 'r') as f:
+    for name in os.listdir(f'{args.input_dir}'):
+        with open(fr'{args.input_dir}\{name}', 'r') as f:
             texts.append(f.read())
 else:
     texts = [i for i in sys.stdin.readlines()]
 for i in texts:
-    sorte = [x.lower() for x in re.findall(r'[а-яА-ЯёЁ]{0,}', i) if x]
+    sorte = [x.lower() for x in re.findall(r'[а-яА-ЯёЁ]{1,}', i)]
     print(sorte)
 

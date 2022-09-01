102d061c0aaa87e5afc23cb4f67828e95a29769f
forIinRanger <ZhuraMax2006@yandex.ru>
2022-09-01 23:05:31 +0500

    shesh

--- train.py
+++ train.py
@@ -2,15 +2,19 @@ import argparse as ap
 import sys
 import os.path
 import re
+import numpy as np
+import pickle
 
 # cd C:\Users\Пользователь\PycharmProjects\Tinkoff
 # считываем передаваемые аргументы из командной строки и переносим их в нашу программу
 parser = ap.ArgumentParser()
-parser.add_argument('--input_dir', type=str, default='stdin')
-parser.add_argument('--model', type=str)
+parser.add_argument('--input_dir', type=str, default='stdin', help='path of texts')
+parser.add_argument('--model', type=str, help='path to file')
 args = parser.parse_args()
 
 file_to_save_model = args.model
+
+# add texts to list from docs
 texts = []
 if args.input_dir != 'stdin':
     for name in os.listdir(f'{args.input_dir}'):
@@ -18,8 +22,40 @@ if args.input_dir != 'stdin':
             texts.append(f.read())
 else:
     texts = [i for i in sys.stdin.readlines()]
+
 sorte = []
 for i in texts:
-    sorte += [x for x in re.findall(r'[а-яА-ЯёЁ]{1,}', i)]
-print(sorte)
+    sorte += [x.lower() for x in re.findall(r'[а-яА-ЯёЁ]+', i)]
+
+
+class Model1:
+    def __init__(self):
+        self.vocabulary = {}
+        self.res = []
+
+    def fit(self, words):
+        for i in range(len(words) - 1):
+            try:
+                self.vocabulary[words[i]] += words[i + 1]
+            except KeyError:
+                self.vocabulary[words[i]] = words[i + 1]
+
+    def generate(self, prefix, length: int):
+        if not prefix:
+            prefix = np.random.choice([i for i in self.vocabulary.keys()])
+        for i in range(length):
+            try:
+                next_word = np.random.choice([i for i in self.vocabulary[prefix]])
+            except KeyError:
+                next_word = np.random.choice([i for i in self.vocabulary.keys()])
+            prefix = next_word
+            self.res += next_word
+        return ''.join(self.res)
+
+
+model = Model1()
+model.fit(sorte)
+with open(f'{file_to_save_model}', 'wb') as f:
+    pickle.dump(model, f)
+
 
